7276bb4531e490d676efa6c8cafd39057daf050c
Jeffrey Fineberg <fineberg@dhcp-10-83-13-60.wireless.buffalo.edu>
2025-08-12 16:40:14 -0400

    simple change2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World2222222!"
+    return "Hello World22222223333333333!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
